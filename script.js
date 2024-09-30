6f6e82ae179b2d81e3709abf234cb668ebc844de
Fut-ball <Arroyave1324@hotmail.com>
2024-09-30 14:55:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+const items = document.querySelectorAll('.list .item');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+let currentIndex = 0;
+
+function showItem(index) {
+    items.forEach((item, i) => {
+        item.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function updateCarousel() {
+    showItem(currentIndex);
+}
+
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + items.length) % items.length;
+    updateCarousel();
+});
+
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % items.length;
+    updateCarousel();
+});
+
+// Inicializar el carrusel
+updateCarousel();
+
+function toggleSections() {
+    const infoSections = document.getElementById('infoSections');
+    infoSections.style.display = infoSections.style.display === 'none' ? 'flex' : 'none';
+    
+}
+
+function showRules() {
+    const rulesSection = document.getElementById('rulesSection');
+    rulesSection.style.display = rulesSection.style.display === 'none' ? 'block' : 'none';
+
+    // Ocultar las posiciones de los jugadores
+    const infoSections = document.getElementById('infoSections');
+    infoSections.style.display = 'none'; 
+
+// Carousel Functionality
+// Step 1: get DOM
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+
+nextDom.onclick = function() {
+    showSlider('next');
+}
+
+prevDom.onclick = function() {
+    showSlider('prev');
+}
+
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    nextDom.click();
+}, timeAutoNext);
+
+function showSlider(type) {
+    let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+
+    if (type === 'next') {
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add('next');
+    } else {
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+        carouselDom.classList.add('prev');
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+        nextDom.click();
+    }, timeAutoNext);
+}
+}
